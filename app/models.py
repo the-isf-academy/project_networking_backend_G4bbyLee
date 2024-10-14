f0de674cf15427b1328f3943640f55166aa36e07
Gabby <0014074@student.isf.edu.hk>
2024-10-14 11:54:44 +0800

    #today I wrote all the models I will need to use in my project and also tested the mvp model in banjo shell. I also started creating the views and added the mvp all. #next lesson, I will work on adding the rest of my mvp views and test them out before starting on the reest of the views.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,41 @@
 # models.py
 
 from banjo.models import Model, StringField, IntegerField, FloatField, BooleanField
+
+class RockClimb(Model):
+    name = StringField()
+    category = StringField()
+    instructions = StringField()
+    extra = StringField()
+    likes = IntegerField()
+    views = IntegerField()
+
+    def json_response(self):       
+        return {
+            'id': self.id,
+            'name': self.name,
+            'category': self.category,
+            'instructions': self.instructions,
+            'extra': self.extra,
+            'likes': self.likes,
+            'views': self.views
+        }
+
+    def increase_likes(self):
+        self.likes += 1
+        self.save()
+
+    def increase_views(self):
+        self.views += 1
+        self.save()
+
+    def change_information(self, name, instructions, extra):
+        self.name = name
+        self.instructions = instructions
+        self.extra = extra
+        self.likes = 0
+        self.save()
+
+    
+
+
--- app/views.py
+++ app/views.py
@@ -1,5 +1,14 @@
 # views.py
 
 from banjo.urls import route_get, route_post
+from .models import RockClimb
 from settings import BASE_URL
 
+@route_get(BASE_URL + 'all')
+def all_fortunes(args):
+    all_list = []
+
+    for fortune in RockClimb.objects.all():
+        all_list.append(.json_response())
+
+    return {'fortunes':fortunes_list}
