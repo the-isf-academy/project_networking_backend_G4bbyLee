7b4cd812d52fbd50f498d99f48c0d6fb05ad3997
Gabby <0014074@student.isf.edu.hk>
2024-10-15 15:30:07 +0800

    #today I continued working on my view mvps, such as adding new exercies, getting singular exercises, and filtering the categories such as the stretches and the exercises. #next lesson, I will work on the rest of my views and fix all the errors in my code.

--- app/views.py
+++ app/views.py
@@ -5,10 +5,58 @@ from .models import RockClimb
 from settings import BASE_URL
 
 @route_get(BASE_URL + 'all')
-def all_fortunes(args):
+def all_moves(args):
     all_list = []
 
-    for fortune in RockClimb.objects.all():
-        all_list.append(.json_response())
+    for climb in RockClimb.objects.all():
+        all_list.append(climb.json_response())
 
-    return {'fortunes':fortunes_list}
+    return {'all excercises and stretches':all_list}
+
+
+@route_post(BASE_URL + 'new', args={'name':str, 'category': str, 'instructions': str, 'extra': str})
+def new_move(args):
+    new_move = RockClimb(
+        name = args['name'],
+        category = args['category'],
+        instructions = args['instructions'],
+        extra = args['extra'],
+        likes = 0,
+        views = 0
+    )
+
+    new_move.save()
+
+    return {'exercise/stretch': new_move.json_response()}
+
+@route_get(BASE_URL + 'one', args={'id': int})
+def one_move(args):
+
+    if RockClimb.objects.filter(id=args['id']).exists():
+        one_move = RockClimb.objects.get(id=args['id'])
+
+        return {'exercise/stretch': one_move.json_response()}
+
+    else:
+        return {'error': 'no exercise/stretch exists'}
+
+@route_get(BASE_URL + 'category', args={'category': str})
+def category(args):
+    if RockClimb.objects.filter(category=args['category']).exists():
+        category = RockClimb.objects.append(category=args['category'])
+
+        return {'category': category.json_response()}
+
+    else:
+        return {'error': 'nothing available'}
+
+@route_post(BASE_URL + 'edit', args={'id': int, 'name': str, 'category': str, 'instructions': str, 'extra': str})
+def edit(args):
+    if RockClimb.objects.filter(id=args['id']).exists():
+        edit = RockClimb.objects.get(id=args['id'])
+        edit.edit(args['name'])
+
+        return{'new exercise/stretch': edit.json_response()}
+
+    else:
+        return {'error': 'no exercise/stretch exists'}
